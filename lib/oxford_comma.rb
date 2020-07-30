675c0e62629acf6a7291b3aadd9e9f6ab28f76ed
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-30 00:27:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   array << "and starfruit"
   array.join(", ")
   elsif array.length > 3
-  array[1].insert(0, " and ")
+  array[-1].insert(0, " and ")
   array.join(", ")
 else array.first
 end
